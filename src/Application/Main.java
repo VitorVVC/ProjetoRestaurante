a0d4f99f3b6a4e477a5860a2db1a51d9cb006b3f
Vitor <vitorvvc2005@gmail.com>
2023-12-01 23:46:02 -0300

    Testando métodos

--- src/Application/Main.java
+++ src/Application/Main.java
@@ -1,10 +1,29 @@
 package Application;
 
 import Model.Entities.Cliente;
-import Model.Entities.Pessoa;
+import Model.Entities.Pedidos;
+
+import java.util.Locale;
+
+import static Application.Util.*;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args){
+        Locale.setDefault(Locale.US);
 
+        // Apresentação !!!
+        // ============= \\
+            principal();
+        // ============= \\
+
+        // TESTES ABAIXO !!!
+        // ============= \\
+        //adicionarAoSistema();
+        Pedidos p = new Pedidos();
+        //p.cardapioGeral();
+        Cliente c = new Cliente();
+        //c.pedido();
+        //c.metodoCriarContaCliente();
+        // ============= \\
     }
 }
